efb77599383adcfff007cae9b5430b2235bbdf32
maksi <maksimbaltak@yandex.ru>
2023-01-27 10:56:51 +0200

    fix

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express()
 app.get('/',(req,res)=>{
     res.send('<h1 style="font-size: 40px">Hello server</h1>')
 })
-app.listen(3000,()=>{
+app.listen(22,'б89.208.196.0',()=>{
     console.log('server start')
 })
